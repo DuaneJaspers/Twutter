37f7af3111cf8fb520a59ec0e0718fdcffd8b47a
Laptop Duane <duanejaspers@gmail.com>
2020-11-08 21:15:17 +0100

    add buttons with snackbar to display message

--- lib/widgets/buttons/logout_button.dart
+++ lib/widgets/buttons/logout_button.dart
@@ -1,21 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class LoginButton extends StatelessWidget {
+class LogoutButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: () {
         _navigateThenDisplayMessage(context);
       },
-      child: Text('Login'),
+      child: Text('Log out'),
     );
   }
 
   void _navigateThenDisplayMessage(BuildContext context) async {
-    final result = await Navigator.pushNamed(context, '/login');
+    FirebaseAuth.instance.signOut();
     Scaffold.of(context)
       ..removeCurrentSnackBar()
-      ..showSnackBar(SnackBar(content: Text("$result")));
+      ..showSnackBar(SnackBar(content: Text("Log out succesfull")));
     Navigator.of(context).pop();
   }
 }
--- lib/widgets/buttons/new_post_fab.dart
+++ lib/widgets/buttons/new_post_fab.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class NewPostFAB extends StatelessWidget {
@@ -13,11 +14,15 @@ class NewPostFAB extends StatelessWidget {
   }
 
   void _pushPostThenDisplayMessage(context) async {
-    final result = await Navigator.pushNamed(context, '/post');
-    if (result != null) {
+    final result = FirebaseAuth.instance.currentUser == null
+        ? await Navigator.pushNamed(context, '/login')
+        : await Navigator.pushNamed(context, '/post');
+    if (result == 'new twuut made!') {
       Scaffold.of(context)
         ..removeCurrentSnackBar()
         ..showSnackBar(SnackBar(content: Text("$result")));
+    } else if (result == 'login successful') {
+      _pushPostThenDisplayMessage(context);
     }
   }
 }

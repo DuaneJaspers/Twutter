6bd967aa454b2ce67a8ac87908c7d0e2db6c4114
Laptop Duane <duanejaspers@gmail.com>
2020-11-09 15:44:12 +0100

    change API to static class

--- lib/API.dart
+++ lib/API.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:twutter/models/post.dart';
+import 'package:twutter/models/profile.dart';
 
 final CollectionReference posts =
     FirebaseFirestore.instance.collection('posts');
-
+final CollectionReference profiles =
+    FirebaseFirestore.instance.collection('profiles');
 // List<Post> getPosts() {
 //   List<Post> postsList = [];
 //   posts.get().then((QuerySnapshot qs) => {
@@ -25,4 +27,30 @@ Future<void> addPost(Post post) {
       })
       .then((value) => print("post added"))
       .catchError((error) => print("Failed to add user: $error"));
+}
+
+Future<void> changePost(Post post) {
+  return posts
+      .doc(post.reference.toString())
+      .set({
+        'content': post.content,
+        'tags': post.tags,
+        'likes': post.likes,
+        'date': post.date,
+        'uid': post.uid,
+      })
+      .then((value) => print("post added"))
+      .catchError((error) => print("Failed to add user: $error"));
+}
+
+Future<void> saveProfile(Profile profile) {
+  return profiles
+      .doc(profile.reference ?? profile.uid)
+      .set({
+        'displayName': profile.displayName,
+        'photoUrl': profile.photoUrl,
+        'following': profile.following,
+      })
+      .then((value) => (print('user added')))
+      .catchError((onError) => print('something failed $onError'));
 }

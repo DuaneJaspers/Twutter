1daaf952155f49421c070282e6c8aaac87e16461
Laptop Duane <duanejaspers@gmail.com>
2020-11-09 15:44:26 +0100

    change API to static class

--- lib/API.dart
+++ lib/API.dart
@@ -2,10 +2,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:twutter/models/post.dart';
 import 'package:twutter/models/profile.dart';
 
-final CollectionReference posts =
-    FirebaseFirestore.instance.collection('posts');
-final CollectionReference profiles =
-    FirebaseFirestore.instance.collection('profiles');
+class API {
+  static final CollectionReference posts =
+      FirebaseFirestore.instance.collection('posts');
+  static final CollectionReference profiles =
+      FirebaseFirestore.instance.collection('profiles');
 // List<Post> getPosts() {
 //   List<Post> postsList = [];
 //   posts.get().then((QuerySnapshot qs) => {
@@ -16,41 +17,42 @@ final CollectionReference profiles =
 //   return postsList;
 // }
 
-Future<void> addPost(Post post) {
-  return posts
-      .add({
-        'content': post.content,
-        'tags': post.tags,
-        'likes': post.likes,
-        'date': post.date,
-        'uid': post.uid,
-      })
-      .then((value) => print("post added"))
-      .catchError((error) => print("Failed to add user: $error"));
-}
+  static Future<void> addPost(Post post) {
+    return posts
+        .add({
+          'content': post.content,
+          'tags': post.tags,
+          'likes': post.likes,
+          'date': post.date,
+          'uid': post.uid,
+        })
+        .then((value) => print("post added"))
+        .catchError((error) => print("Failed to add user: $error"));
+  }
 
-Future<void> changePost(Post post) {
-  return posts
-      .doc(post.reference.toString())
-      .set({
-        'content': post.content,
-        'tags': post.tags,
-        'likes': post.likes,
-        'date': post.date,
-        'uid': post.uid,
-      })
-      .then((value) => print("post added"))
-      .catchError((error) => print("Failed to add user: $error"));
-}
+  static Future<void> changePost(Post post) {
+    return posts
+        .doc(post.reference.toString())
+        .set({
+          'content': post.content,
+          'tags': post.tags,
+          'likes': post.likes,
+          'date': post.date,
+          'uid': post.uid,
+        })
+        .then((value) => print("post added"))
+        .catchError((error) => print("Failed to add user: $error"));
+  }
 
-Future<void> saveProfile(Profile profile) {
-  return profiles
-      .doc(profile.reference ?? profile.uid)
-      .set({
-        'displayName': profile.displayName,
-        'photoUrl': profile.photoUrl,
-        'following': profile.following,
-      })
-      .then((value) => (print('user added')))
-      .catchError((onError) => print('something failed $onError'));
+  static Future<void> saveProfile(Profile profile) {
+    return profiles
+        .doc(profile.reference ?? profile.uid)
+        .set({
+          'displayName': profile.displayName,
+          'photoUrl': profile.photoUrl,
+          'following': profile.following,
+        })
+        .then((value) => (print('user added')))
+        .catchError((onError) => print('something failed $onError'));
+  }
 }

2a21be37006a41889da2e6862d239740f89d4434
Laptop Duane <duanejaspers@gmail.com>
2020-11-11 09:11:16 +0100

    changed profile with async way to get profile info

--- lib/views/profile.dart
+++ lib/views/profile.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:twutter/API.dart';
+import 'package:twutter/models/profile.dart';
+import 'package:twutter/widgets/buttons/follow_button.dart';
 import 'package:twutter/widgets/twuutList.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -8,30 +11,63 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  String displayName = 'profile';
+  String displayName;
   bool ownProfile = false;
+  String _uid;
+  Profile _userProfile;
+  bool loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _getProfileAsync(String uid) async {
+    if (_uid == FirebaseAuth.instance.currentUser.uid) {
+      if (mounted) {
+        setState(() {
+          displayName = FirebaseAuth.instance.currentUser.displayName;
+          ownProfile = true;
+          loading = false;
+        });
+        return;
+      }
+    }
+    Profile profile = Profile.fromSnapshot(await API.profiles.doc(uid).get());
+    Profile userProfile = Profile.fromSnapshot(
+        await API.profiles.doc(FirebaseAuth.instance.currentUser.uid).get());
+    if (mounted) {
+      setState(() {
+        _userProfile = userProfile;
+        displayName = profile.displayName;
+        loading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    String uid = ModalRoute.of(context).settings.arguments.toString();
+    _uid = ModalRoute.of(context).settings.arguments.toString();
+    _getProfileAsync(_uid);
 
-    if (uid == FirebaseAuth.instance.currentUser.uid) {
-      setState(() {
-        displayName = FirebaseAuth.instance.currentUser.displayName;
-        ownProfile = true;
-      });
-    }
     return Scaffold(
       appBar: AppBar(
         title: Text('profile'),
       ),
       body: Column(
         children: [
-          Text('$displayName'),
+          (loading
+              ? CircularProgressIndicator()
+              : Column(
+                  children: [
+                    Text('$displayName'),
+                    if (!ownProfile) FollowButton(_uid, _userProfile),
+                  ],
+                )),
           Expanded(
               child: SizedBox(
                   child: TwuutList(
-            filters: [uid],
+            filters: [_uid],
           ))),
           // TwuutList(),
         ],
